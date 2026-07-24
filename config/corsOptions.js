eeedef2916380b35c7973ab343b173ea3c30a9dc
Mokhfi Mohammed <abalahab370@gmail.com>
2026-07-25 00:24:41 +0100

    fixed Cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,13 +2,8 @@ const cors = require("cors") ;
 
 //Cross Origin Resource Sharing :
 const whitelist = [
-      'https://www.google.com',
-      'http://127.0.0.1:5500',
-      'http://localhost:5500',
-      'http://127.0.0.1:5501',
-      'http://localhost:5501',
-      'http://127.0.0.1:3000',
-      'http://localhost:3000'
+      'https://blogger-project-n6s6.onrender.com',
+      'http://127.0.0.1:5500'
 ];
 
 const corsOptions = {
